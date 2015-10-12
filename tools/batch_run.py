801604de7053b7533120fc9ac9ede2a75e4da9f8
HW-Lee <tony123930@yahoo.com.tw>
2015-10-12 14:45:39 +0800

    Add tools: gen_run_space, batch_run to run tasks with different conditions

---
+++ tools/batch_run.py
@@ -0,0 +1,82 @@
+import os
+import sys
+import time
+import getopt
+import glob
+import re
+
+def main( argv ):
+
+	try:
+		opts, args = getopt.getopt( argv[1:], "h", [ "dir=" ] )
+		opts = dict( opts )
+	except getopt.GetoptError as err:
+		opts = {}
+		opts[ "-h" ] = ""
+
+	if "-h" in opts.keys() or "--dir" not in opts.keys():
+		print_usage()
+		exit(0)
+
+	if opts[ "--dir" ].endswith( "/" ): opts[ "--dir" ] = opts[ "--dir" ][0:-1]
+
+	sh_list = glob.glob( opts[ "--dir" ] + "/*.sh" )
+	resources_list = map( _get_resources, sh_list )
+	job_names_list = map( _get_job_name, sh_list )
+
+	pkg_list = zip( sh_list, map( lambda x: [x[0]] + x[1],zip( job_names_list, resources_list ) ) )
+
+	single_node_list = filter( lambda x: x[1][1] is 1, pkg_list )
+	multi_node_list = filter( lambda x: x[1][1] > 1, pkg_list )
+
+	map( _print, single_node_list )
+	print( "------------------------" )
+	map ( _print, multi_node_list )
+
+	map( _dummy_run_script, single_node_list )
+	print( "" )
+
+	exit(0)
+
+
+def print_usage():
+	print( "Usage: -h for help, --dir DIR_PATH" )
+
+def _get_resources( path ):
+	job_str = ""
+	with open( path, "r" ) as f: job_str = f.read()
+	nodes_num, ppn = re.sub( "[(nodes=)(ppn=)]", "", re.search( "nodes=[0-9]+?:ppn=[0-9]+?", job_str ).group( 0 ) ).split( ":" )
+
+	return [ int( nodes_num ), int( ppn ) ]
+
+def _get_job_name( path ):
+	job_str = ""
+	with open( path, "r" ) as f: job_str = f.read()
+	name = re.sub( "-N ", "", re.search( "-N .*?\w+", job_str ).group( 0 ) )
+
+	return name
+
+def _dummy_run_script( pkg ):
+	path = pkg[0]
+	job_name = pkg[1][0]
+	nodes_num = pkg[1][1]
+	ppn = pkg[1][2]
+
+	print( "Running " + path + " ..." )
+	os.system( "qsub " + path + " > /dev/null" )
+
+	while len( glob.glob( job_name + ".*" ) ) < 2: time.sleep( .2 )
+
+	_process_file( glob.glob( job_name + ".o*" )[0], job_name, nodes_num, ppn )
+	os.system( "rm " + job_name + ".*" )
+
+def _process_file( path, job_name, nodes_num, ppn ):
+	file_name = str( job_name ) + "_nodes=" + str( nodes_num ) + ":ppn=" + str( ppn ) + ".txt"
+	os.system( "cp " + path + " ./" + file_name )
+	print( "generate report: " + file_name )
+
+def _print( arg ):
+	print( arg )
+	return None
+
+if __name__ == "__main__": main( sys.argv )
---
+++ tools/gen_run_space.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import opt_processing
+import gen_job_script as job
+import json
+import shutil
+import numpy as np
+
+def main(argv):
+    import getopt
+
+    OPTS = [
+        ("h", ""),
+        ("dir", "="),
+        ("config", "=")
+    ]
+    USAGE = "\n".join( map( lambda x: "".join(x), OPTS ) )
+
+    try:
+        opts, args = opt_processing.process_opts( argv[1:], OPTS, USAGE )
+    except getopt.GetoptError as err: print( err )
+
+    if "h" in opts.keys() or "config" not in opts.keys():
+        print( USAGE )
+        exit(0)
+
+    out_dir = opts.get( "dir", "run_space" )
+    if out_dir.endswith( "/" ): out_dir = out_dir[0:-1]
+    config_file = file( opts[ "config" ] )
+
+    if os.path.exists( out_dir ) and os.path.isdir( out_dir ): shutil.rmtree( out_dir )
+    os.makedirs( out_dir )
+
+    config = json.load( config_file )
+    queue_mode = config[ "queue_mode" ]
+    job_name = config[ "job_name" ]
+    rerunnable = config[ "rerunnable" ]
+    exec_cmd = config[ "exec_cmd" ]
+    outpath_prefix = config[ "outpath_prefix" ]
+
+    res_configs = config[ "res_configs" ]
+    n = len( res_configs )
+    outpaths = map( lambda x: "/".join( [ out_dir, outpath_prefix + str(x) + ".sh" ] ), np.arange( 0, n ) + 1 )
+    map( _dummy_gen_job_script, [queue_mode] * n, [job_name] * n, [rerunnable] * n, [exec_cmd] * n, outpaths, res_configs )
+
+    exit(0)
+
+def _dummy_gen_job_script( queue_mode, job_name, rerunnable, exec_cmd, outpath, res_config ):
+    nodes_num = str( res_config[ "nodes_num" ] )
+    ppn = str( res_config[ "ppn" ] )
+    wt = str( res_config[ "wt" ] )
+
+    job.gen_job_script( queue_mode, job_name, rerunnable, nodes_num, ppn, wt, exec_cmd, outpath)
+
+
+if __name__ == "__main__": main(sys.argv)
