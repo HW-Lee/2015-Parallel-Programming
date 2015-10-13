9f0299ca64ee78d31763359ee9e615dd66fc3905
HW-Lee <tony123930@yahoo.com.tw>
2015-10-13 23:55:54 +0800

    [advanced sort] time probing function added, stable release v1

--- HW01/HW1_103061527_advanced.c
+++ HW01/HW1_103061527_advanced.c
@@ -59,12 +59,21 @@ int main( int argc, char* argv[] ) {
     int* data_buf = malloc( ( size + red_process_num ) * ( data_bufsize / size ) * sizeof( int ) );
     MPI_File f;
 
+    start = clock();
+    MPI_Barrier( MPI_COMM_WORLD );
+    end = clock();
+    sync_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
+
+    start = clock();
     // MPI_File_open (MPI_Comm comm, char *filename, int amode, MPI_Info info, MPI_File *fh)
     MPI_File_open( MPI_COMM_WORLD, in_file, MPI_MODE_RDONLY, MPI_INFO_NULL, &f );
 
     // MPI_File_read (MPI_File fh, void *buf, int count, MPI_Datatype datatype, MPI_Status *status)
     MPI_File_read( f, data_buf, N, MPI_INT, MPI_STATUS_IGNORE );
+    end = clock();
+    IO_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
 
+    start = clock();
     int i;
     for (i = 0; i < data_bufsize - N; i++) data_buf[ N + i ] = INT_MAX;
 
@@ -77,6 +86,8 @@ int main( int argc, char* argv[] ) {
     }
 
     if ( rank != ROOT ) free( data_buf );
+    end = clock();
+    comp_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
 
     int updated = 1;
     int local_updated;
@@ -86,6 +97,12 @@ int main( int argc, char* argv[] ) {
 
         local_updated = 0;
 
+        start = clock();
+        MPI_Barrier( MPI_COMM_WORLD );
+        end = clock();
+        sync_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
+
+        start = clock();
         if ( rank < size ) {
             if ( rank > ROOT ) {
                 send_msg = local_buf[0];
@@ -95,7 +112,15 @@ int main( int argc, char* argv[] ) {
                 MPI_Recv( &recv_msg, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
             }
         }
+        end = clock();
+        comm_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
 
+        start = clock();
+        MPI_Barrier( MPI_COMM_WORLD );
+        end = clock();
+        sync_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
+
+        start = clock();
         if ( rank < size && rank < size-1 ) {
             send_msg = recv_msg;
             if ( compare( recv_msg, local_buf[ local_bufsize-1 ] ) < 1 ) {
@@ -104,12 +129,23 @@ int main( int argc, char* argv[] ) {
                 local_buf[ local_bufsize-1 ] = recv_msg;
             }
         }
+        end = clock();
+        comp_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
 
+        start = clock();
+        MPI_Barrier( MPI_COMM_WORLD );
+        end = clock();
+        sync_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
+
+        start = clock();
         if ( rank < size ) {
             if ( rank < size-1 ) MPI_Send( &send_msg, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD );
             if ( rank > ROOT ) MPI_Recv( &recv_msg, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
         }
+        end = clock();
+        comm_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
 
+        start = clock();
         if ( rank < size && rank > ROOT ) {
             shift_array( local_buf, local_bufsize, 1, DIR_LEFT );
             local_buf[ local_bufsize-1 ] = recv_msg;
@@ -123,17 +159,41 @@ int main( int argc, char* argv[] ) {
             insert_and_kick( local_buf, local_bufsize, &send_msg );
             insert_and_kick( local_buf, local_bufsize, &recv_msg );
         }
+        end = clock();
+        comp_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;;
 
+        start = clock();
+        MPI_Barrier( MPI_COMM_WORLD );
+        end = clock();
+        sync_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
+
+        start = clock();
         MPI_Allreduce( &local_updated, &updated, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD );
+        end = clock();
+        comm_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
     }
 
+    start = clock();
+    MPI_Barrier( MPI_COMM_WORLD );
+    end = clock();
+    sync_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
+
+    start = clock();
     // MPI_Gather(sendbuf, sendcount, sendtype, recvbuf, recvcount, recvtype, root, comm)
     MPI_Gather( local_buf, local_bufsize, MPI_INT, data_buf, local_bufsize, MPI_INT, ROOT, MPI_COMM_WORLD );
+    end = clock();
+    comm_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
 
+    start = clock();
     if ( rank == ROOT ) {
         FILE* f = fopen( out_file, "wb" );
         fwrite( data_buf, sizeof( int ), N, f );
     }
+    end = clock();
+    IO_millis += ((double) (end - start)) * 1000 / CLOCKS_PER_SEC;
+
+    printf( "{\n\t\"id\": %d,\n\t\"i/o\": %.3f,\n\t\"comm\": %.3f,\n\t\"sync\": %.3f\n\t\"comp\": %.3f\n}\n", 
+        rank, IO_millis, comm_millis, sync_millis, comp_millis );
 
     MPI_Barrier( MPI_COMM_WORLD );
     MPI_Finalize();
