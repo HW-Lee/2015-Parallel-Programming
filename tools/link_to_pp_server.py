f7216984eac30509bc51bbbeffa0a0208e26ae26
HW-Lee <tony123930@yahoo.com.tw>
2015-11-02 17:38:31 +0800

    [link_pp_server] add options api

--- tools/link_to_pp_server.py
+++ tools/link_to_pp_server.py
@@ -1,11 +1,15 @@
 import os
+import sys
 
-def main():
+def main(argv):
     
     usr = "s103061527"
 
-    cmd = "ssh " + usr + "@" + "140.114.91.171"
+    opt = ""
+    if len(argv) > 1: opt = argv[1]
+
+    cmd = "ssh " + opt + " " + usr + "@" + "140.114.91.171"
     os.system( cmd )
 
 
-if __name__ == '__main__': main()
+if __name__ == '__main__': main(sys.argv)
