bb7abf0fdb0d31537ae9cd5e5c5cb5a5925f40c0
HW-Lee <tony123930@yahoo.com.tw>
2015-12-19 22:14:11 +0800

    [HW4] cuda-only/cuda-omp: 1th finished <tested>

--- HW04/HW4_103061527_openmp.cu
+++ HW04/HW4_103061527_openmp.cu
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <cuda_runtime.h>
+#include <omp.h>
 
 #define OUTFILE 1
 #define INF (int) 1 << 24
@@ -11,9 +12,9 @@ __device__ __host__ int ij2ind(int i, int j, int N) {
 }
 
 int* Dist;
-int* Dist_d;
+int** Dist_dt;
 
-__global__ void updateList(int* D, int blocksize, int N, int r, int blockDimWidth, int phase) {
+__global__ void updateList(int* D, int blocksize, int N, int r, int blockDimWidth, int phase, int rowIdx) {
     int bi, bj;
     switch(phase) {
         case 0:
@@ -30,8 +31,8 @@ __global__ void updateList(int* D, int blocksize, int N, int r, int blockDimWidt
             }
             break;
         case 2:
-            bi = (r + blockIdx.y + 1) % (int) ceil((double) N/blocksize);
-            bj = (r + blockIdx.x + 1) % (int) ceil((double) N/blocksize);
+            bi = (r + rowIdx + 1) % (int) ceil((double) N/blocksize);
+            bj = (r + blockIdx.y + 1) % (int) ceil((double) N/blocksize);
             break;
     }
     extern __shared__ int DS[];
@@ -43,9 +44,7 @@ __global__ void updateList(int* D, int blocksize, int N, int r, int blockDimWidt
     // DS[0:bibs-1][:] = B[bi][bj] = D[bibs:(bi+1)bs-1][bjbs:(bj+1)bs-1]
     // DS[bibs:2bibs-1][:] = B[bi][r] = D[bibs:(bi+1)bs-1][rbs:(r+1)bs-1]
     // DS[2bibs:3bibs-1][:] = B[r][bi] = D[rbs:(r+1)bs-1][bjbs:(bj+1)bs-1]
-    #pragma unroll 2
     for (int i = threadIdx.y; i < blocksize; i+=blockDimWidth) {
-        #pragma unroll 2
         for (int j = threadIdx.x; j < blocksize; j+=blockDimWidth) {
             DS[ij2ind(i, j, blocksize)] = D[ij2ind(i+offset_i, j+offset_j, N)];
             DS[ij2ind(i+blocksize, j, blocksize)] = D[ij2ind(i+offset_i, j+offset_r, N)];
@@ -56,9 +55,7 @@ __global__ void updateList(int* D, int blocksize, int N, int r, int blockDimWidt
 
     // DS[i][j] = min{ DS[i][j], DS[i+bs][k] + DS[k+2bs][j] }
     for (int k = 0; k < blocksize; k++) {
-        #pragma unroll 2
         for (int i = threadIdx.y; i < blocksize; i+=blockDimWidth) {
-            #pragma unroll 2
             for (int j = threadIdx.x; j < blocksize; j+=blockDimWidth) {
                 if (DS[ij2ind(i, j, blocksize)] > DS[ij2ind(i+blocksize, k, blocksize)] + DS[ij2ind(k+2*blocksize, j, blocksize)]) {
                     DS[ij2ind(i, j, blocksize)] = DS[ij2ind(i+blocksize, k, blocksize)] + DS[ij2ind(k+2*blocksize, j, blocksize)];
@@ -70,9 +67,7 @@ __global__ void updateList(int* D, int blocksize, int N, int r, int blockDimWidt
         __syncthreads();
     }
 
-    #pragma unroll 2
     for (int i = threadIdx.y; i < blocksize; i+=blockDimWidth) {
-        #pragma unroll 2
         for (int j = threadIdx.x; j < blocksize; j+=blockDimWidth) {
             // DS[i][j] = D[i+bsbi][j+bsbj]
             D[ij2ind(i+offset_i, j+offset_j, N)] = DS[ij2ind(i, j, blocksize)];
@@ -85,6 +80,16 @@ int main(int argc, char* argv[]) {
     cudaSetDevice(dev);
     cudaDeviceProp dp;
     cudaGetDeviceProperties(&dp, dev);
+
+    int num_dev = 1;
+    cudaGetDeviceCount(&num_dev);
+    #pragma omp parallel num_threads(num_dev)
+    {
+        cudaSetDevice(omp_get_thread_num());
+        int gpuid = -1;
+        cudaGetDevice(&gpuid);
+        printf("Thread%d gets GPU%d\n", omp_get_thread_num(), gpuid);
+    }
 
     int blockDimWidth = (int) sqrt(dp.maxThreadsPerBlock);
 
@@ -115,7 +120,12 @@ int main(int argc, char* argv[]) {
 
     // TODO: Allocate memory (pinned/unpinned)
     cudaMallocHost((void**) &Dist, sizeof(int) * N_ext*N_ext);
-    cudaMalloc((void**) &Dist_d, sizeof(int) * N_ext*N_ext);
+    Dist_dt = (int**) malloc(sizeof(int*) * num_dev);
+    #pragma omp parallel num_threads(num_dev)
+    {
+        cudaSetDevice(omp_get_thread_num());
+        cudaMalloc((void**) &Dist_dt[omp_get_thread_num()], sizeof(int) * N_ext*N_ext);
+    }
 
     for (int i = 0; i < N_ext; ++i) {
         for (int j = 0; j < N_ext; ++j) {
@@ -132,9 +142,6 @@ int main(int argc, char* argv[]) {
     }
     fclose(infile);
 
-    // TODO: Copy values loaded from the file
-    cudaMemcpy((void*) Dist_d, (void*) Dist, sizeof(int) * N_ext*N_ext, cudaMemcpyHostToDevice);
-
     // TODO: Updating list
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
@@ -147,37 +154,60 @@ int main(int argc, char* argv[]) {
 
     int num_blocks_per_column = (int) ceil((double) N_ext/blocksize);
     dim3 grid_1(2, num_blocks_per_column-1);
-    dim3 grid_2(num_blocks_per_column-1, num_blocks_per_column-1);
-    for (int r = 0; r < num_blocks_per_column; r++) {
-        printf("\rCompute progress: %.2f%%", (float) r/num_blocks_per_column*100);
+    dim3 grid_2(1, num_blocks_per_column-1);
 
-        cudaEventRecord(start);
-        updateList<<< 1, block, sizeof(int) * 3*blocksize*blocksize >>>(Dist_d, blocksize, N_ext, r, blockDimWidth, 0);
-        cudaEventRecord(stop);
-        cudaEventSynchronize(stop);
-        cudaEventElapsedTime(&t, start, stop);
-        phase1elapsed_millis += t;
+    #pragma omp parallel num_threads(num_dev)
+    {
+        int t_id = omp_get_thread_num();
+        cudaSetDevice(t_id);
 
-        cudaEventRecord(start);
-        updateList<<< grid_1, block, sizeof(int) * 3*blocksize*blocksize >>>(Dist_d, blocksize, N_ext, r, blockDimWidth, 1);
-        cudaEventRecord(stop);
-        cudaEventSynchronize(stop);
-        cudaEventElapsedTime(&t, start, stop);
-        phase2elapsed_millis += t;
+        for (int r = 0; r < num_blocks_per_column; r++) {
+            if (t_id == 0) printf("\rCompute progress: %.2f%%", (float) r/num_blocks_per_column*100);
 
-        cudaEventRecord(start);
-        updateList<<< grid_2, block, sizeof(int) * 3*blocksize*blocksize >>>(Dist_d, blocksize, N_ext, r, blockDimWidth, 2);
-        cudaEventRecord(stop);
-        cudaEventSynchronize(stop);
-        cudaEventElapsedTime(&t, start, stop);
-        phase3elapsed_millis += t;
+            cudaMemcpy((void*) Dist_dt[t_id], (void*) Dist, sizeof(int) * N_ext*N_ext, cudaMemcpyHostToDevice);
 
-        printf("\rCompute progress: %.2f%%", (float) (r+1)/num_blocks_per_column*100);
+            if (t_id == 0) cudaEventRecord(start);
+            updateList<<< 1, block, sizeof(int) * 3*blocksize*blocksize >>>(Dist_dt[t_id], blocksize, N_ext, r, blockDimWidth, 0, -1);
+            if (t_id == 0) {
+                cudaEventRecord(stop);
+                cudaEventSynchronize(stop);
+                cudaEventElapsedTime(&t, start, stop);
+                phase1elapsed_millis += t;
+            }
+
+            if (t_id == 0) cudaEventRecord(start);
+            updateList<<< grid_1, block, sizeof(int) * 3*blocksize*blocksize >>>(Dist_dt[t_id], blocksize, N_ext, r, blockDimWidth, 1, -1);
+            if (t_id == 0) {
+                cudaEventRecord(stop);
+                cudaEventSynchronize(stop);
+                cudaEventElapsedTime(&t, start, stop);
+                phase2elapsed_millis += t;
+            }
+
+            if (t_id == 0) cudaEventRecord(start);
+            if (t_id == 0) cudaMemcpy((void*) Dist, (void*) Dist_dt[0], sizeof(int) * N_ext*N_ext, cudaMemcpyDeviceToHost);
+            #pragma omp barrier
+
+            cudaMemcpy((void*) Dist_dt[t_id], (void*) Dist, sizeof(int) * N_ext*N_ext, cudaMemcpyHostToDevice);
+            #pragma omp for schedule(dynamic)
+            for (int i = 0; i < num_blocks_per_column-1; i++) {
+                updateList<<< grid_2, block, sizeof(int) * 3*blocksize*blocksize >>>(Dist_dt[t_id], blocksize, N_ext, r, blockDimWidth, 2, i);
+                int idx = ij2ind( ((r+1+i) % num_blocks_per_column)*blocksize, 0, N_ext );
+                cudaMemcpy((void*) &(Dist[idx]), (void*) &(Dist_dt[t_id][idx]), sizeof(int)*N_ext*blocksize, cudaMemcpyDeviceToHost);
+            }
+            if (t_id == 0) {
+                cudaEventRecord(stop);
+                cudaEventSynchronize(stop);
+                cudaEventElapsedTime(&t, start, stop);
+                phase3elapsed_millis += t;
+            }
+
+            if (t_id == 0) printf("\rCompute progress: %.2f%%", (float) (r+1)/num_blocks_per_column*100);
+
+            #pragma omp barrier
+        }
     }
     printf("\n");
-
-    // TODO: Copy final values
-    cudaMemcpy((void*) Dist, (void*) Dist_d, sizeof(int) * N_ext*N_ext, cudaMemcpyDeviceToHost);
 
     // TODO: Write file
     if (OUTFILE == 1) {
@@ -197,7 +227,11 @@ int main(int argc, char* argv[]) {
 
     // TODO: Free memory
     cudaFreeHost(Dist);
-    cudaFree(Dist_d);
+    #pragma omp parallel num_threads(num_dev)
+    {
+        cudaSetDevice(omp_get_thread_num());
+        cudaFree(Dist_dt[omp_get_thread_num()]);
+    }
 
     return 0;
 }
